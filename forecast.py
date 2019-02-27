85aff601811c6d05352f72697161b18037ca79fc
Unknown <cerulean.connor@gmail.com>
2019-02-27 14:56:12 +0000

    Added state for future addons such as refresh()

--- forecast.py
+++ forecast.py
@@ -19,12 +19,23 @@ class Forecast():
         'flags',
         'latitude',
         'longitude',
-        'timezone'
+        'timezone',
+        'headers',
+        'raw',
+        '_state'
     )
-    def __init__(self, latitude, longitude, timezone, **optional_params):
+    def __init__(self, state, headers, latitude, longitude, timezone, **optional_params):
+        self._state = state
+
         self.latitude = latitude
         self.longitude = longitude
         self.timezone = timezone
+        self.headers = headers # unprocessed
+        self.raw = optional_params
+        self.raw['latitude'] = latitude
+        self.raw['longitude'] = longitude
+        self.raw['timezone'] = timezone
+        
         for key, value in optional_params.items():
             if key in IS_DATABLOCK:
                 setattr(self, key, DataBlock(**value))
